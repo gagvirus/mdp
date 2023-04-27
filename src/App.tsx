7c6bfc3fbe0b8c68c57bf27e79cb9dd9346ac1e7
GagVirus <gagvirus92@gmail.com>
2023-04-28 01:24:52 +0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from "react";
 import "./App.scss";
 import data from "./data.json";
-import {Item} from "./models";
+import {Item, Rarity} from "./models";
 
 function App() {
 	const [items, setItems] = useState<Item[]>([]);
+	const [level, setLevel] = useState(1);
+	const [clientPos, setClientPos] = useState<[number?, number?]>([])
+	const [displayValue, setDisplayValue] = useState<number>();
 
 
 	useEffect(() => {
@@ -19,7 +22,17 @@ function App() {
 		setItems(newItems);
 	}, []);
 
-	const RARITIES = ["common", "uncommon", "rare", "epic"];
+	const RARITIES: Rarity[] = ["common", "uncommon", "rare", "epic"];
+
+	const bla = (evt: React.MouseEvent<HTMLDivElement>, item: Item, rarity: Rarity) => {
+		setClientPos([evt.clientX, evt.clientY])
+		setDisplayValue(Math.ceil(item.getValueForLevel(level, rarity)))
+	}
+
+	const unBla = () => {
+		setClientPos([])
+		setDisplayValue(undefined);
+	}
 
 	return (
 		<div className="App">
@@ -27,16 +40,23 @@ function App() {
 				{items.map((item: Item, itemKey) => (
 					<div className="item-wrapper" key={itemKey}>
 						{RARITIES.map((rarity, rarityKey) => (
-							<div className={`item item-rarity ${rarity}`} key={rarityKey}>
-								<img src={item.getImageUrl()} alt={`${item.name}-${rarity}`} />
+							<div className={`item item-rarity ${rarity}`} key={rarityKey}
+									 onMouseEnter={(evt) => bla(evt, item, rarity)}
+									 // onMouseLeave={unBla}
+							>
+								<img src={item.getImageUrl()} alt={`${item.name}-${rarity}`}/>
 							</div>
 						))}
 					</div>
 				))}
 			</div>
 			<div className="level-slider">
-				<input type="text"/>
+				<input type="number" value={level} onChange={evt => setLevel(parseInt(evt.target.value))} min={1} max={80}/>
 			</div>
+			{clientPos.length && displayValue && <div className="value-wrapper" style={{
+				left: clientPos[0],
+				top: clientPos[1]
+			}}>{displayValue}</div>}
 		</div>
 	);
 }
--- src/models.ts
+++ src/models.ts
@@ -1,7 +1,13 @@
+const LEVEL_COEFFICIENT = 1.05;
+const RARITY_COEFFICIENT = 1.2;
+
+export type Rarity = "common" | "uncommon" | "rare" | "epic";
+
 class Item {
     public type: string;
     public name: string;
     public value: number;
+
     constructor(type: string, name: string, value: number) {
         this.type = type;
         this.name = name;
@@ -10,6 +16,20 @@ class Item {
 
     getImageUrl() {
         return `/images/${this.name}.png`;
+    }
+
+    getValueForLevel(level: number, rarity: Rarity) {
+        return Math.ceil(this.value * Math.pow(LEVEL_COEFFICIENT, (level - 1))) * this.getRarityMultiplier(rarity);
+    }
+
+    getRarityMultiplier(rarity: Rarity) {
+        const rarities = {
+            common: 1,
+            uncommon: 2,
+            rare: 3,
+            epic: 4,
+        }
+        return Math.pow(RARITY_COEFFICIENT, rarities[rarity] - 1);
     }
 }
 
