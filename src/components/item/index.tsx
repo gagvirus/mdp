22e4cbdb27a25ea01cc60f1e7ddd2fce9f0ec76b
GagVirus <gagvirus92@gmail.com>
2023-04-30 16:01:11 +0400

    mock modal

--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -1,5 +1,5 @@
 import {Item as ItemModel, Rarity} from "../../models";
-import React from "react";
+import React, {useState} from "react";
 import "./index.scss";
 
 interface ItemProps {
@@ -9,10 +9,31 @@ interface ItemProps {
 }
 
 function Item({item, rarity, level}: ItemProps) {
-	return <div className={`item item-rarity ${rarity}`}>
-		<img src={item.getImageUrl()} alt={`${item.name}-${rarity}`}/>
-		<p className="level">{level}</p>
-	</div>
+	const [detailsOpen, setDetailsOpen] = useState(false);
+
+	return <>
+		<div className={`item item-rarity ${rarity}`} onClick={() => setDetailsOpen(true)}>
+			<img src={item.getImageUrl()} alt={`${item.name}-${rarity}`}/>
+			<p className="level">{level}</p>
+		</div>
+		{detailsOpen && <div className="modal fade show" style={{display: "block"}}>
+			<div className="modal-dialog">
+				<div className="modal-content">
+					<div className="modal-header">
+						<h5 className="modal-title">Modal title</h5>
+						<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div className="modal-body">
+						<p>Modal body text goes here.</p>
+					</div>
+					<div className="modal-footer">
+						<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+						<button type="button" className="btn btn-primary">Save changes</button>
+					</div>
+				</div>
+			</div>
+		</div>}
+	</>
 }
 
 export default Item;
