c743a8a898602488f66309e77de9232b4cc22657
GagVirus <gagvirus92@gmail.com>
2023-04-30 15:43:41 +0400

    inventory

---
+++ src/components/inventory/index.tsx
@@ -0,0 +1,13 @@
+function Inventory() {
+	return <div className="row">
+		<div className="col-2">
+			weapons
+		</div>
+		<div className="col-8"></div>
+		<div className="col-2">
+			armor
+		</div>
+	</div>
+}
+
+export default Inventory;
