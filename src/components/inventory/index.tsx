b596e21481d1a619dbc02b950eab7ccff7315548
GagVirus <gagvirus92@gmail.com>
2023-05-01 13:25:08 +0400

    total spent

--- src/components/inventory/index.tsx
+++ src/components/inventory/index.tsx
@@ -1,5 +1,5 @@
 import ItemPlaceholder from "../item-placeholder/item-placeholder";
-import {EquipmentType, Item as ItemModel} from "../../models";
+import {Dict, EquipmentType, Item as ItemModel} from "../../models";
 import Item from "../item";
 import {useEquipment} from "../../context/equipment-context";
 
@@ -8,14 +8,37 @@ function ItemOrPlaceholder({item, type}: {item: ItemModel | null, type: Equipmen
 }
 
 function Inventory() {
+  const {getItem} = useEquipment();
+
   const weapons: EquipmentType[] = ["main", "special", "ultimate", "launcher"];
   const armor: EquipmentType[] = ["helmet", "gloves", "armor", "boots"];
-  const {getItem} = useEquipment();
+  const allEquipment = weapons.concat(armor);
+  let totalMoneySpent = 0;
+  let tokensSpent: Dict<number> = {};
+  allEquipment.forEach((type) => {
+    const equippedItem = getItem(type);
+    if (equippedItem) {
+      totalMoneySpent += equippedItem.getTotalMoneyCost();
+      const ts = equippedItem.getTotalTokenCost();
+      if (ts) {
+        if (['main', 'special'].includes(type)) {
+          tokensSpent[equippedItem.name] = ts;
+        } else {
+          tokensSpent[equippedItem.type] = ts;
+        }
+      }
+    }
+  })
   return <div className="row">
     <div className="col-2">
       {weapons.map((type) => <ItemOrPlaceholder item={getItem(type)} type={type} key={type} />)}
     </div>
-    <div className="col-8"></div>
+    <div className="col-8">
+      <ul>
+        {totalMoneySpent && <li>Total Money Spent: {totalMoneySpent}</li>}
+        {Object.keys(tokensSpent).map((key: string) => <li key={key}>{key} Tokens Spent: {tokensSpent[key]}</li>)}
+      </ul>
+    </div>
     <div className="col-2">
       {armor.map((type) => <ItemOrPlaceholder item={getItem(type)} type={type} key={type} />)}
     </div>
