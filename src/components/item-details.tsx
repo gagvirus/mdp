1231ad79a06e3609ba36ebd749b8e8d621db0634
GagVirus <gagvirus92@gmail.com>
2023-05-01 12:02:18 +0400

    moved details to separate component

--- src/components/item-details.tsx
+++ src/components/item-details.tsx
@@ -1,5 +1,54 @@
-function ItemDetails() {
+import ItemPreview from "./item-preview/item-preview";
+import Slider from "rc-slider";
+import Modal from "./modal";
+import React from "react";
+import {Item as ItemModel} from "../models";
 
+interface ItemDetailsProps {
+  item: ItemModel;
+  handleClose: () => void;
+  handleLevelChange: (level: number | number[]) => void;
+}
+
+function ItemDetails({item, handleClose, handleLevelChange}: ItemDetailsProps) {
+  return <Modal title={item.name} show={true} onClose={handleClose} showClose={true} showSave={false}>
+    <div className="row">
+      <div className="col-10">
+        <div style={{width: 200, margin: "auto"}}>
+          <ItemPreview item={item} />
+        </div>
+        <ul>
+          <li>
+            Attribute: {Math.ceil(item.getValueForLevel())}
+          </li>
+          <li>
+            Upgrade Cost: {item.getMoneyCost()}
+          </li>
+          <li>
+            Upgrade Tokens Cost: {item.getTokenCost()}
+          </li>
+          <li>
+            Total Upgrade Cost: {item.getTotalMoneyCost()}
+          </li>
+          <li>
+            Total Upgrade Tokens Cost: {item.getTotalTokenCost()}
+          </li>
+        </ul>
+      </div>
+      <div className="col-2">
+        <Slider
+          vertical
+          style={{height: 400}}
+          max={item.getMaxLevelForRarity()}
+          min={1}
+          startPoint={1}
+          step={1}
+          onChange={handleLevelChange}
+          marks={item.getMarksForRarity()}
+        />
+      </div>
+    </div>
+  </Modal>
 }
 
 export default ItemDetails;
--- src/components/item/index.tsx
+++ src/components/item/index.tsx
@@ -1,9 +1,8 @@
 import {Item as ItemModel} from "../../models";
 import React, {useState} from "react";
 import "./index.scss";
-import Modal from "../modal";
-import Slider from "rc-slider";
 import ItemPreview from "../item-preview/item-preview";
+import ItemDetails from "../item-details";
 
 interface ItemProps {
   item: ItemModel;
@@ -19,45 +18,11 @@ function Item({item, handleLevelChange}: ItemProps) {
 
   return <>
     <ItemPreview item={item} setDetailsOpen={setDetailsOpen}/>
-    {detailsOpen &&
-      <Modal title={item.name} show={true} onClose={() => setDetailsOpen(false)} showClose={true} showSave={false}>
-        <div className="row">
-          <div className="col-10">
-            <div style={{width: 200, margin: "auto"}}>
-              <ItemPreview item={item} />
-            </div>
-            <ul>
-              <li>
-                Attribute: {Math.ceil(item.getValueForLevel())}
-              </li>
-              <li>
-                Upgrade Cost: {item.getMoneyCost()}
-              </li>
-              <li>
-                Upgrade Tokens Cost: {item.getTokenCost()}
-              </li>
-              <li>
-                Total Upgrade Cost: {item.getTotalMoneyCost()}
-              </li>
-              <li>
-                Total Upgrade Tokens Cost: {item.getTotalTokenCost()}
-              </li>
-            </ul>
-          </div>
-          <div className="col-2">
-            <Slider
-              vertical
-              style={{height: 400}}
-              max={item.getMaxLevelForRarity()}
-              min={1}
-              startPoint={1}
-              step={1}
-              onChange={(n) => handleSliderChange(n)}
-              marks={item.getMarksForRarity()}
-            />
-          </div>
-        </div>
-      </Modal>}
+    {detailsOpen && <ItemDetails
+      item={item}
+      handleLevelChange={handleSliderChange}
+      handleClose={() => setDetailsOpen(false)}
+    />}
   </>
 }
 
