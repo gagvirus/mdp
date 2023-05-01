154adc4c30df4df099b593b5db21c3312c5dfcf9
GagVirus <gagvirus92@gmail.com>
2023-05-01 12:55:06 +0400

    using context

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import "./App.scss";
 import ItemsWrapper from "./components/items-wrapper";
 import Inventory from "./components/inventory";
 import {ItemsContextProvider} from "./context/items-context";
+import {EquipmentContextProvider} from "./context/equipment-context";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <ItemsContextProvider>
-          <Inventory/>
+          <EquipmentContextProvider>
+            <Inventory/>
 
-          <ItemsWrapper/>
+            <ItemsWrapper/>
+          </EquipmentContextProvider>
+
         </ItemsContextProvider>
       </div>
     </div>
